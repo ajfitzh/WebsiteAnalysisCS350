96ed26337b478d0818b03d334854477158451f16
Austin James Fitzhugh afitz010 <austin.j.fitzhugh@gmail.com>
2022-06-29 16:02:48 -0400

    added testwebsite

---
+++ BasicSite/JS/audio.js
@@ -0,0 +1,17 @@
+var yourAudio = document.getElementById('yourAudio'),
+    ctrl = document.getElementById('audioControl');
+
+ctrl.onclick = function () {
+
+    // Update the Button
+    var pause = ctrl.innerHTML === 'pause!';
+    ctrl.innerHTML = pause ? 'play!' : 'pause!';
+
+    // Update the Audio
+    var method = pause ? 'pause' : 'play';
+    yourAudio[method]();
+
+    // Prevent Default Action
+    return false;
+};
+
