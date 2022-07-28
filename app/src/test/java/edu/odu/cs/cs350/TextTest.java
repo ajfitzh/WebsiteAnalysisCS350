6242b45fead7130158b78de4862940bac57218e8
ty-j-schiffer <ty.j.schiffer@gmail.com>
2022-07-28 05:23:00 -1000

    Update TextTest.java
    For Formatting, although it's generating a text file

--- app/src/test/java/edu/odu/cs/cs350/TextTest.java
+++ app/src/test/java/edu/odu/cs/cs350/TextTest.java
@@ -3,6 +3,9 @@ package edu.odu.cs.cs350;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TextTest {
      @Test public void TextOutputExist() {
@@ -12,6 +15,15 @@ public class TextTest {
     assertNotNull("Text class should generate filename output", fileName);
 
     
+}
+
+@Test public void TextOutputStructure() {
+
+    String fileName = null;
+    Format f = new SimpleDateFormat("MMddyyyy-hhmmss");
+    String date = f.format(new Date());
+    fileName = date+"-summary.txt";
+    assertEquals(fileName, Text.output());
 }
 
 }
