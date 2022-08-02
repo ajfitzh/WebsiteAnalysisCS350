6cdfff4e3a24326f9b6594ed07b47647b67869e0
Abdullah Pathan, cs_apath003 <apath003@odu.edu>
2022-08-01 23:54:15 -0400

    build Excel output from Website pages, update Page object

--- app/src/main/java/edu/odu/cs/cs350/Excel.java
+++ app/src/main/java/edu/odu/cs/cs350/Excel.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
@@ -13,56 +14,70 @@ import org.apache.poi.hssf.usermodel.*;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
-
-
 
 public class Excel {
 	//This class is called by OutputPackager and retrieves info from Website, Pages, and others
 	// in order to output a .xlsx file
 	//NEEDS: # of images/CSS style references, JS scripts, intra-page/external/internal links
-	
-	 
 	//string for filename, must have format YYYMMDD-hhmmss-summary.xlsx
 	String filename;
 	//output function for .xlsx file
-	public static String output() {
-	    Format f = new SimpleDateFormat("MMddyyyy-hhmmss");
-	    String date = f.format(new Date());
-	    String fileName = date + "-summary.xls";
-//		XSSFWorkbook workbook = new XSSFWorkbook();
+	public static String output(Website website) {
+	    String fileName = getFileName();
 		try(HSSFWorkbook workbook = new HSSFWorkbook()) {
 			HSSFSheet firstSheet = workbook.createSheet("summary");
-
-			
-			//HSSFRow rowA = firstSheet.createRow(0);
-			//HSSFCell cellA = rowA.createCell(0);
-			//cellA.setCellValue(new HSSFRichTextString("Website Analysis"));
-			
 			HSSFRow row;
-
 			Map<String, Object[]> webSiteData = new TreeMap<String, Object[]>();
 			
 			// first generic row of summary spreadsheet
 			webSiteData.put("1", 
-				new Object[] {"Page", "#Images", "#CSS", "Scripts", "#Links (Intra-Page)", "#Links (Internal)", "#Links (External)"});
+				new Object[] {"Page", "# Images", "# CSS", "Scripts", "# Links (Intra-Page)", "# Links (Internal)", "# Links (External)"});
+
+			int pageNumber = 1;
+			
+			for(Page page : website.pages){
+				String key = Integer.toString(pageNumber + 1);
+				webSiteData.put(key, 
+					new Object[] {
+						page.name,
+						(!(page.images == null || page.images.isEmpty()) ? Integer.toString(page.images.size()) : "0"),
+						(!(page.css == null || page.css.isEmpty()) ? Integer.toString(page.css.size()) : "0"),
+						(!(page.javascript == null || page.javascript.isEmpty()) ? Integer.toString(page.javascript.size()) : "0"),
+						Integer.toString(page.intraLinks),
+						Integer.toString(page.intersiteLinks), 
+						Integer.toString(page.externalLinks)
+					});
+				pageNumber++;
+			}
 
 			try(FileOutputStream fos = new FileOutputStream(getFileName())) {
 
 			HSSFRow rowA = firstSheet.createRow(0);
 			HSSFCell cellA = rowA.createCell(0);
 			cellA.setCellValue(new HSSFRichTextString("Website Analysis"));
-			try(FileOutputStream fos1 = new FileOutputStream(fileName)) {
 
-				workbook.write(fos1);
-				workbook.close();
+			Set<String> keyid = webSiteData.keySet();
+			int rowid = 0;
+	
+			for (String key : keyid) {
+	
+				row = firstSheet.createRow(rowid++);
+				Object[] objectArr = webSiteData.get(key);
+				int cellid = 0;
+	
+				for (Object obj : objectArr) {
+					HSSFCell cell = row.createCell(cellid++);
+					cell.setCellValue((String)obj);
+				}
+			}
+			workbook.write(fos);
+			workbook.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return fileName;
+			return fileName;
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -72,8 +87,8 @@ public class Excel {
 
 	// Generate filename for .xlsx file
 	public static String getFileName() {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyymmdd-hhmmss");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMddyyyy-hhmmss");
    		LocalDateTime now = LocalDateTime.now();
-		return dtf.format(now) + "-summary.xlsx";
+		return dtf.format(now) + "-summary.xls";
 	}
 }
--- app/src/main/java/edu/odu/cs/cs350/Page.java
+++ app/src/main/java/edu/odu/cs/cs350/Page.java
@@ -1,5 +1,6 @@
 package edu.odu.cs.cs350;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.io.File;
 import java.io.IOException;
@@ -30,66 +31,90 @@ public class Page {
         this.path =  path;
         System.out.println("-Page " + name + " " + id + "path: " + path + " created-");
         htmlExtractor(path);
-
     }
 	//array holding unique ID's of all CSS scripts referenced by this page
-	Collection<CSS> css;
+	Collection<CSS> css = new ArrayList<CSS>();;
 	//array holding unique ID's of all Javascript scripts referenced by this page
-	Collection<JavaScript> javascript;
+	Collection<JavaScript> javascript = new ArrayList<JavaScript>();;
 	//Array holding unique ID's of all Images referenced by this page
-	Collection<Image> images;
+	Collection<Image> images = new ArrayList<Image>();;
 	//Array holding unique ID's of all Links referenced by this page
-	Collection<Page> links;
-	
+	Collection<Link> links = new ArrayList<Link>();
+	// Number of intrapage, external and intersite links
+	int intraLinks = 0;
+	int externalLinks = 0;
+	int intersiteLinks = 0;
 
+	//HTML Extractor extracts anchor tags/links and classifies links as intra-page, intra-site, or external
+	//Extracts Image tags and classifies as internal/external images
+	//Extracts file size for each image, the URI for each image, and local pages on which it is referenced
+	public void htmlExtractor(String htmlfile) {
 		
-
-	
-		//HTML Extractor extracts anchor tags/links and classifies links as intra-page, intra-site, or external
-		//Extracts Image tags and classifies as internal/external images
-		//Extracts file size for each image, the URI for each image, and local pages on which it is referenced
-		public void htmlExtractor(String htmlfile) {
+		int intrapageLinks = 0;
+		int intersiteLinks = 0;
+		int externalLinks = 0;
+		System.out.println("----Start JSoup Analysis---");
+		//IF it's an HTML file, use JSoup to decipher HTML objects in it
+		File input = new File(htmlfile);
+		try {
+			Document doc = Jsoup.parse(input, "UTF-8", "");
 			
-			int intrapageLinks = 0;
-			int intersiteLinks = 0;
-			int externalLinks = 0;
-			System.out.println("----Start JSoup Analysis---");
-			//IF it's an HTML file, use JSoup to decipher HTML objects in it
-			File input = new File(htmlfile);
-			try {
-				Document doc = Jsoup.parse(input, "UTF-8", "");
-				
-				System.out.println("--Links--");
-				Elements links = doc.select("a[href]");
-				for(Element link : links) {
-					System.out.println(link.attr("href"));
-					if(link.attr("href").contains("#")) { intrapageLinks++; }
-					else if(link.attr("href").contains("http")) { externalLinks++; }
-					else { intersiteLinks++; }
+			System.out.println("--Links--");
+			Elements links = doc.select("a[href]");
+			for(Element link : links) {
+				System.out.println(link.attr("href"));
+				if(link.attr("href").contains("#")) { 
+					intrapageLinks++;
+					this.links.add(new Link(link.normalName(), link.id(), link.baseUri(), "intrapage"));
 				}
-				
-				System.out.println("--Image References--");
-				Elements imagereferences = doc.select("img[src~=(?i)\\.(svg|png|jpe?g|gif)]");
-				for(Element image : imagereferences) {
-					System.out.println(image.attr("src"));
+				else if(link.attr("href").contains("http")) { 
+					externalLinks++;
+					this.links.add(new Link(link.normalName(), link.id(), link.baseUri(), "external"));
 				}
-				
-				System.out.println("--CSS/JS External References--");
-				Elements cssreferences = doc.select("link");
-				for(Element cssreference : cssreferences) {
-					System.out.println(cssreference.attr("href"));
-					System.out.println(cssreference.attr("src"));
+				else { 
+					intersiteLinks++;
+					this.links.add(new Link(link.normalName(), link.id(), link.baseUri(), "intersite"));
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			System.out.println("Intra-page Links: " + intrapageLinks );
-			System.out.println("Inter-site Links: " + intersiteLinks );
-			System.out.println("External Links  : " + externalLinks );
-				System.out.println("--End of JSoup Analysis--");
+			
+			System.out.println("--Image References--");
+			Elements imagereferences = doc.select("img[src~=(?i)\\.(svg|png|jpe?g|gif)]");
+			for(Element image : imagereferences) {
+				System.out.println(image.attr("src"));
+				this.images.add(new Image(image.normalName(), (int) Math.random(), 0, 0, image.baseUri()));
+			}
+			
+			System.out.println("--CSS/JS External References--");
+			Elements cssreferences = doc.select("link");
+			for(Element cssreference : cssreferences) {
+				System.out.println(cssreference.attr("href"));
+				System.out.println(cssreference.attr("src"));
+				this.css.add(new CSS(cssreference.normalName(), (int) Math.random(), 0, 0, cssreference.baseUri()));
+			}
+
+			System.out.println("--JS References--");
+			Elements jsFiles = doc.select("script");
+			for(Element js : jsFiles) {
+				System.out.println(js.attr("href"));
+				System.out.println(js.attr("src"));
+				this.javascript.add(new JavaScript(js.normalName(), (int) Math.random(), 0, 0, js.baseUri()));
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		this.intraLinks = intrapageLinks;
+		this.externalLinks = externalLinks;
+		this.intersiteLinks = intersiteLinks;
+
+		System.out.println("Intra-page Links: " + intrapageLinks );
+		System.out.println("Inter-site Links: " + intersiteLinks );
+		System.out.println("External Links  : " + externalLinks );
+			System.out.println("--End of JSoup Analysis--");
 	}
+}
 		
 	
 	
